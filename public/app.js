7df34161d8ca59b024781bf0a79af36d562172d8
Aaron Persaud <aaronpersaud@hotmail.com>
2016-09-20 08:59:08 +0000

    added modal for login

--- public/app.js
+++ public/app.js
@@ -1,8 +1,29 @@
 angular
   .module('MyApp',['ngMaterial' ])
-  .controller('DemoCtrl', function($scope) {
+  .controller('DemoCtrl', function($scope, $mdDialog) {
     $scope.user = {
       title: 'Enter your questions',
+    };
+    $scope.status = '  ';
+    $scope.customFullscreen = false;
+
+    $scope.showPrompt = function(ev) {
+      // Appending dialog to document.body to cover sidenav in docs app
+      var confirm = $mdDialog.prompt()
+        .title('Please Login')
+        //.textContent('Bowser is a common name.')
+        .placeholder('Username')
+        .ariaLabel('Dog name')
+        //.initialValue('Buddy')
+        .targetEvent(ev)
+        .ok('Okay!')
+        .cancel('Anonymous');
+
+      $mdDialog.show(confirm).then(function(result) {
+        $scope.status = 'You are logged in as ' + result + '.';
+      }, function() {
+        $scope.status = 'You are not logged in';
+      });
     };
   })
   .config(function($mdThemingProvider) {
@@ -12,5 +33,9 @@ angular
     $mdThemingProvider.theme('docs-dark', 'default')
       .primaryPalette('yellow')
       .dark();
+
+  })
+
+  .controller('AppCtrl', function($scope, $mdDialog) {
 
   });
